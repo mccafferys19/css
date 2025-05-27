3274dd7b318d78ef6a7e4401a3189a13f7071012
Stephen McCaffery <stephen.mccaffery@kellogg.northwestern.edu>
2025-05-27 16:23:20 +0000

    end of class

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,12 @@ class PostsController < ApplicationController
     # render :template => "posts/index"
 
     # alternative responses to requests other than HTML
-    # respond_to do |format|
-    #   format.html # implicitly renders template posts/index.html.erb
-    #   format.json do
-    #     render :json => @posts
-    #   end
-    # end
+    respond_to do |format|
+      format.html # implicitly renders template posts/index.html.erb
+      format.json do
+        render :json => @posts
+      end
+    end
   end
 
   def new
@@ -30,11 +30,11 @@ class PostsController < ApplicationController
     redirect_to "/posts"
   end
 
-  # before_action :allow_cors
-  # def allow_cors
-  #   response.headers['Access-Control-Allow-Origin'] = '*'
-  #   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-  #   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
-  #   response.headers['Access-Control-Max-Age'] = '1728000'
-  # end
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end
 end
